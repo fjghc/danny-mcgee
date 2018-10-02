7e281fc1395eb621ef27ca0c1e0b63bcee1217c7
dannymcgee <dannymcgee@gmail.com>
2018-10-01 23:12:00 -0400

    Header implemented

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'dm-header',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  urlTitleLookup = {
+    '/': 'Home',
+    '/projects': 'Projects',
+    '/login': 'Login'
+  };
+  pageTitle: string;
+
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.router.events.subscribe(
+      event => {
+        if (event instanceof RoutesRecognized) {
+          console.log(event.url.toString());
+          this.pageTitle = this.urlTitleLookup[event.url.toString()];
+        }
+      }
+    );
+  }
+
+  onNewProject() {
+    this.router.navigate(['/new'], { relativeTo: this.route });
+  }
+
+  onEditProjects() {
+
   }
 
 }

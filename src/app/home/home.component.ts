7b0ac115a9be37b80e026be8b0c5464be640df7c
dannymcgee <dannymcgee@gmail.com>
2018-10-06 13:45:41 -0400

    Removed ngx-monaco-editor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,28 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'dm-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  monacoOptions = {
-    theme: 'vs-dark',
-    language: 'typescript',
-    readOnly: true,
-  };
-  code = 'function x() {\n    console.log("Hello world!");\n}';
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {}
-
-  onGetToken = () => console.log(this.authService.getToken());
-
-  onUpdateCode() {
-    this.code = 'console.log(\'Hello world!\');';
-  }
+  constructor() {}
 
 }

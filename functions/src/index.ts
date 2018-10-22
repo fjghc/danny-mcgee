c426e2706c6fcdee97913b11934d6a21849ebc2c
dannymcgee <dannymcgee@gmail.com>
2018-10-22 00:46:33 -0400

    Added cloud function to send email from Contact form

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,30 @@
 import * as functions from 'firebase-functions';
+import * as nodemailer from 'nodemailer';
+import * as cors from 'cors';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const corsHandler = cors({ origin: true });
+const gmailEmail = functions.config().gmail.email;
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+export const sendMail = functions.https.onRequest((request, response) => {
+  corsHandler(request, response, () => {
+
+    return Promise.resolve()
+      .then(() => {
+        if (request.method !== 'POST') {
+          const error = new Error('Only POST requests are accepted') as any;
+          error.code = 405;
+          throw error;
+        }
+
+        return mailTransport.sendMail(request.body)
+          .then(_response => {
+            response.send(_response);
+          })
+          .catch(error => {
+            response.send(error);
+          });
+      });
+  });
+});
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { faEnvelope } from '@fortawesome/pro-light-svg-icons';
 
@@ -17,8 +18,9 @@ export class ContactComponent implements OnInit {
   icons = {
     mail: faEnvelope
   };
+  contactInfoType: 'email' | 'phone' | 'other' = null;
 
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
   ngOnInit() {
     this.form = new FormGroup({});
@@ -26,6 +28,33 @@ export class ContactComponent implements OnInit {
 
   onSubmit() {
     console.log(this.form.value);
+
+    const fromEmail = this.contactInfoType === 'email' ? this.form.value.contactInfo : 'dannymcgee.io@gmail.com';
+    const emailContent = `<p><b>Name:</b><br /> ${this.form.value.name}</p>`
+      + `<p><b>Contact Info:</b><br /> ${this.form.value.contactInfo}</p>`
+      + `<p><b>Message:</b><br />${this.form.value.message}</p>`;
+
+    console.log('Email body:', emailContent);
+
+    const url = 'https://us-central1-danny-mcgee.cloudfunctions.net/sendMail';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      })
+    };
+
+    const body = JSON.stringify({
+      to: 'dannymcgee@gmail.com',
+      from: `"${this.form.value.name}" <${fromEmail}>`,
+      subject: 'New message from dannymcgee.io Contact form',
+      text: '',
+      html: emailContent
+    });
+
+    return this.httpClient.post(url, body, httpOptions).toPromise()
+      .then(response => console.log(response))
+      .catch(error => console.log('ERROR calling cloud function:', error));
   }
 
 }

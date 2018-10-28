acc3001569720dd642ebbd6be4827bea0e3d8f8b
dannymcgee <dannymcgee@gmail.com>
2018-10-28 17:15:21 -0400

    Finished Home page; misc styling tweaks

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,62 +7,6 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  // files = [
-  //   {
-  //     name: 'css',
-  //     type: 'folder',
-  //     contents: [
-  //       {
-  //         name: 'styles.css',
-  //         type: 'css',
-  //         path: 'dannymcgee/src/css/styles.css'
-  //       },
-  //     ]
-  //   },
-  //   {
-  //     name: 'js',
-  //     type: 'folder',
-  //     contents: [
-  //       {
-  //         name: 'test',
-  //         type: 'folder',
-  //         contents: [
-  //           {
-  //             name: 'test.js',
-  //             type: 'javascript',
-  //             path: 'dannymcgee/src/js/test/test.js'
-  //           }
-  //         ]
-  //       },
-  //       {
-  //         name: 'test.js',
-  //         type: 'javascript',
-  //         path: 'dannymcgee/src/js/test.js'
-  //       },
-  //       {
-  //         name: 'script.js',
-  //         type: 'javascript',
-  //         path: 'dannymcgee/src/js/script.js'
-  //       },
-  //       {
-  //         name: 'mock-data.ts',
-  //         type: 'typescript',
-  //         path: 'dannymcgee/src/js/mock-data.ts'
-  //       }
-  //     ]
-  //   },
-  //   {
-  //     name: 'index.html',
-  //     type: 'htmlmixed',
-  //     path: 'dannymcgee/src/index.html'
-  //   },
-  //   {
-  //     name: 'heresastupidlongfilenamefortestingloremipsumdolorsitametconsecteturadipiscingelit.html',
-  //     type: 'htmlmixed',
-  //     path: 'dannymcgee/src/heresastupidlongfilenamefortestingloremipsumdolorsitametconsecteturadipiscingelit.html'
-  //   }
-  // ];
-
   constructor() {}
 
 }

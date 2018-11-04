d2326cbc6797031a1c5c2fa3e2068966756beb41
dannymcgee <dannymcgee@gmail.com>
2018-11-04 10:42:56 -0500

    Fixed issue with AOT compiler compatibility

--- src/app/shared/animations/fade-out-in.animation.ts
+++ src/app/shared/animations/fade-out-in.animation.ts
@@ -3,23 +3,19 @@ import { fadeConfig } from './animation.configs';
 import { fadeOutQuery } from './fade-out.animation';
 import { fadeInQuery } from './fade-in.animation';
 
-export function fadeOutIn(elem1Selector: string, elem2Selector?: string): AnimationMetadata {
-  const initialStep: AnimationMetadata = elem2Selector ? query(
+export function fadeOutIn(elem1Selector: string, elem2Selector: string): AnimationMetadata {
+  return sequence([
+    query(
       elem2Selector,
       style({ position: 'fixed', transform: `translateY(${fadeConfig.distance})`, opacity: 0 }),
       { optional: true }
-    ) : null;
-
-  const middleStep: AnimationMetadata = elem2Selector ? query(
-    elem2Selector,
-    style({ position: 'relative', transform: `translateY(${fadeConfig.distance})` }),
-    { optional: true }
-  ) : null;
-
-  return sequence([
-    initialStep ? initialStep : null,
+    ),
     fadeOutQuery(elem1Selector),
-    middleStep ? middleStep : null,
-    fadeInQuery(elem2Selector ? elem2Selector : elem1Selector)
+    query(
+      elem2Selector,
+      style({ position: 'relative', transform: `translateY(${fadeConfig.distance})` }),
+      { optional: true }
+    ),
+    fadeInQuery(elem2Selector)
   ]);
 }

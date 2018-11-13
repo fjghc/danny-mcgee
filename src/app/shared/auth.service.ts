78e6fee03237fc63bd059383866925d19e5fed7e
dannymcgee <dannymcgee@gmail.com>
2018-11-12 23:47:20 -0500

    Switched Firebase into production mode

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,7 +3,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 // Dependency imports
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 // App imports
